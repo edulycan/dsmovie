26a4d608f05c3d5a33d2fb99107b585fd02b40a0
Eduardo de Oliveira <eduardolycan@gmail.com>
2022-01-16 16:17:23 -0300

    _redirects

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -35,8 +35,8 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <Link to="/">                
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
                 </Link>
             </div >
         </div >
